40eea574dd371e19003a736f1d3b81ae6053169b
Adam Palmer <acpalmer1011@gmail.com>
2023-01-23 19:27:31 +0000

    Implemented tableView and button functionality

---
+++ iTahDoodle/iTahDoodle/TodoList.swift
@@ -0,0 +1,33 @@
+//
+//  TodoList.swift
+//  iTahDoodle
+//
+//  Created by Adam Palmer on 23/01/2023.
+//
+
+import UIKit
+
+class TodoList: NSObject {
+    fileprivate var items: [String] = []
+    
+    
+    func add(_ item: String) {
+        items.append(item)
+    }
+}
+
+extension TodoList: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let item = items[indexPath.row]
+        
+        cell.textLabel!.text = item
+        
+        return cell
+    }
+}
--- iTahDoodle/iTahDoodle/ViewController.swift
+++ iTahDoodle/iTahDoodle/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var itemTextField: UITextField!
+    @IBOutlet var tableView: UITableView!
+    
+    let todoList = TodoList()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.dataSource = todoList
     }
 
+    @IBAction func insertButtonPressed(_ sender: UIButton) {
+        guard let todo = itemTextField.text else {
+            return
+        }
+        todoList.add(todo)
+        tableView.reloadData()
+    }
 
 }
 
