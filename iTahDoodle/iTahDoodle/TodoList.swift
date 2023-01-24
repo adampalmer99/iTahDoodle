1e8cd10c782209cca08d3f15854a5c5cc825c83c
Adam Palmer <acpalmer1011@gmail.com>
2023-01-24 14:58:29 +0000

    Implement save on list

--- iTahDoodle/iTahDoodle/TodoList.swift
+++ iTahDoodle/iTahDoodle/TodoList.swift
@@ -8,11 +8,38 @@
 import UIKit
 
 class TodoList: NSObject {
+    private let fileURL: URL = {
+        let documentDirectoryURLs = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        var documentDirectoryURL = documentDirectoryURLs.first!
+        return documentDirectoryURL.appendingPathComponent("todolist.items")
+    }()
+   
     fileprivate var items: [String] = []
     
+    override init() {
+        super.init()
+        loadItems()
+    }
+    
+    func saveItems() {
+        let itemsArray = items as NSArray
+        
+        print("Saving items to \(fileURL)")
+        if !itemsArray.write(to: fileURL, atomically: true) { // Attempts to save contents of array, bool = succeeded or not
+            print("Could not save to-do list")
+        }
+    }
+    
+    // If array can be constructed, can cast the array to [String], store it in TodoList's items property.
+    func loadItems() {
+        if let itemsArray = NSArray(contentsOf: fileURL) as? [String] { // attempt to construct NSArray using its initialiser which expects a URL from which the array should be loaded.
+            items = itemsArray
+        }
+    }
     
     func add(_ item: String) {
         items.append(item)
+        saveItems()
     }
 }
 
